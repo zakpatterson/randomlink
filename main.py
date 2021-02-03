b00eef221e62060b31a4cf42cce45ae00ecce392
Zak Patterson <pattersonzak@gmail.com>
2021-02-03 23:35:17 +0900

    Add another url

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 links = [
     '//www.gsdigitalcookie.com/cookie/landing/3/d4bb81a4-2c2b-40c1-a6a1-afdd8e5af9fc',
     '//www.gsdigitalcookie.com/cookie/landing/3/ad7357b0-ca9a-4668-ab99-2386e5389e5b',
-    '//www.gsdigitalcookie.com/cookie/landing/0/e027908f-0c06-4cc7-a3d3-6ce2b44684b5'
+    '//www.gsdigitalcookie.com/cookie/landing/0/e027908f-0c06-4cc7-a3d3-6ce2b44684b5',
+    '//www.gsdigitalcookie.com/cookie/landing/0/f8663d89-eacd-4b5d-b448-5d01c65b5bc1'
 ]
 
 
