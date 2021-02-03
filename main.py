effc6546ff67c1630221a546ec8a8d9162c08318
Zak Patterson <pattersonzak@gmail.com>
2021-02-03 10:50:04 +0900

    Update links

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import random
 app = Flask(__name__)
 
 links = [
-    '//bing.com',
-    '//google.com',
-    '//yahoo.com',
-    '//cnn.com'
+    '//www.gsdigitalcookie.com/cookie/landing/3/d4bb81a4-2c2b-40c1-a6a1-afdd8e5af9fc',
+    '//www.gsdigitalcookie.com/cookie/landing/3/ad7357b0-ca9a-4668-ab99-2386e5389e5b',
+    '//www.gsdigitalcookie.com/cookie/landing/0/e027908f-0c06-4cc7-a3d3-6ce2b44684b5'
 ]
 
 
