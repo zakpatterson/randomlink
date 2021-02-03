22febff5050229aaec129992ea8003a1ff5d475c
Zak Patterson <pattersonzak@gmail.com>
2021-02-03 10:39:03 +0900

    Add initial code

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect
+import random
+app = Flask(__name__)
+
+links = [
+    '//bing.com',
+    '//google.com',
+    '//yahoo.com',
+    '//cnn.com'
+]
+
+
+@app.route('/')
+def random_link():
+    redirect(random.choice(links))
