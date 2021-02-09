a31014d25fe8c3ec9dc2642e3bc362aa8103751a
Zak Patterson <pattersonzak@gmail.com>
2021-02-10 08:06:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ links = [
     '//www.gsdigitalcookie.com/cookie/landing/3/d4bb81a4-2c2b-40c1-a6a1-afdd8e5af9fc',
     '//www.gsdigitalcookie.com/cookie/landing/3/ad7357b0-ca9a-4668-ab99-2386e5389e5b',
     '//www.gsdigitalcookie.com/cookie/landing/0/e027908f-0c06-4cc7-a3d3-6ce2b44684b5',
-    '//www.gsdigitalcookie.com/cookie/landing/0/f8663d89-eacd-4b5d-b448-5d01c65b5bc1'
+    '//www.gsdigitalcookie.com/cookie/landing/0/f8663d89-eacd-4b5d-b448-5d01c65b5bc1',
+    '//www.gsdigitalcookie.com/cookie/landing/0/6daabe19-6cf4-496e-8a78-9689618ebe78'
 ]
 
 
